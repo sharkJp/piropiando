da3812c90e445a7ac7af8063fa039205a22df1ba
jhonge <jhonserviteck@gmail.com>
2026-01-24 09:07:42 -0500

    cambios en sw.js

--- sw.js
+++ sw.js
@@ -39,3 +39,10 @@ self.addEventListener("fetch", event => {
     )
   );
 });
+self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
